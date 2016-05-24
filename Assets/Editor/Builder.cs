cb6ac37a0586d2c1209fcb027474bf5bf631a3ee
ONODA Teruaki <whitedev.support@gmail.com>
2016-05-24 12:17:11 +0900

    ビルドテスト

---
+++ Assets/Editor/Builder.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class Builder {
+
+    [UnityEditor.MenuItem("Tools/Build for Android")]
+    public static void BuildForAndroid() {
+
+        EditorUserBuildSettings.SwitchActiveBuildTarget( BuildTarget.Android );
+
+        List<string> allScenes = new List<string>();
+        foreach( EditorBuildSettingsScene scene in EditorBuildSettings.scenes ){
+            if (scene.enabled) {
+                allScenes.Add (scene.path);
+            }
+        }   
+
+//        PlayerSettings.bundleIdentifier = "com.unity.app";
+//        PlayerSettings.statusBarHidden = true;
+        BuildPipeline.BuildPlayer( 
+            allScenes.ToArray(),
+            "app.apk",
+            BuildTarget.Android,
+            BuildOptions.None
+        );
+    }
+}
